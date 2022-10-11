8bf45d6b4ea502d8603694a9597bee471d693cbc
kojosh <111550404+kojosh@users.noreply.github.com>
2022-10-10 23:49:51 -0700

    quiz 6-1

--- q1.cpp
+++ q1.cpp
@@ -6,6 +6,11 @@ using namespace std;
 // Declare Function Prototypes
 //********************
 
+void getinput(int&, int&);
+void getinput(float&, float&);
+void swapTwoValues(int&, int&);
+void swapTwoValues(float&, float&);
+
 int main()
 {
 	int num1, num2;
@@ -17,11 +22,38 @@ int main()
 
 	// -- Call overloaded function with float parameters
 	getinput(fnum1, fnum2);
-	cout << setw(5) << fnum1 << setw(5) << fnum2 << setw(5) << endl;
+	cout << fixed << setprecision(2);
+	cout << " Before Swap" << setw(5) << fnum1 << setw(5) << fnum2 << setw(5) << endl;
 	swapTwoValues(fnum1, fnum2);
-	cout << setw(5) << fnum1 << setw(5) << fnum2 << setw(5) << endl;
+	cout << " After Swap" << setw(5) << fnum1 << setw(5) << fnum2 << setw(5) << endl;
 }
 
 // ******************************
 // Implement all your functions here
 // ******************************
+
+void getinput(int& a, int& b)
+{
+	cout << "Enter two integers: ";
+	cin >> a >> b;
+}
+
+void getinput (float& a, float& b)
+{
+	cout << "Enter two floating values: ";
+	cin >> a >> b;
+}
+
+void swapTwoValues(int& a, int& b)
+{
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
+void swapTwoValues(float& a, float& b)
+{
+	float tmp = a;
+	a = b;
+	b = tmp;
+}
